93eb81ea816be0dfcb7996217475d00c1eed80ef
Jared Crace <Jared.Crace@mantech.com>
2022-01-23 22:14:41 -0600

    exhange class and wallet class working

--- client/index.js
+++ client/index.js
@@ -18,10 +18,13 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
 document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
+  const signature = document.getElementById("send-signature").value;
   const recipient = document.getElementById("recipient").value;
 
+  console.log(`111from client: ${signature}`);
+
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender, amount, signature, recipient
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
--- server/index.js
+++ server/index.js
@@ -7,9 +7,6 @@ const ec = new EC('secp256k1');
 
 const port = 3042;
 
-const key1 = ec.genKeyPair();
-const key2 = ec.genKeyPair();
-const key3 = ec.genKeyPair();
 
 function printKey(keyMsg, key) {
   console.log(`key msg: ${keyMsg}`)
@@ -21,42 +18,121 @@ function printKey(keyMsg, key) {
   });
 }
 
-//printKey('key1', key1);
-//printKey('key2', key2);
-//printKey('key3', key3);
-
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
-const addr1 = key1.getPublic().encode('hex').slice(128,130);
-const addr2 = key2.getPublic().encode('hex').slice(128,130);
-const addr3 = key3.getPublic().encode('hex').slice(128,130);
+class Wallet {
+  constructor(key, amount) {
+    this.key = key;
+    this.address = key.getPublic().encode('hex').slice(128,130);
+    this.amount = amount;
+  }
 
-
-const balances = {
-  ['_' + addr1]: 100,
-  ['_' + addr2]: 50,
-  ['_' + addr3]: 75,
+  print(){
+    console.log(this.key, this.address, this.amount);
+  }
 }
 
-console.log(balances);
+class Exchange {
+  constructor() {
+    this.walletObjList = [];
+    this.walletStrList = [];
+  }
+
+  createNewWallet(startAmount) {
+    const key = ec.genKeyPair();
+    const newWallet = new Wallet(key, startAmount);
+
+    this.walletObjList.push(newWallet);
+    this.walletStrList.push(newWallet.address);
+  }
+
+  isValidWalletAddress(address) {
+    if(this.walletStrList.length == 0) {
+      return;
+    }
+
+    if(address.length != this.walletStrList[0].length){
+      console.log(`address wrong size: ${address}`);
+      return;
+    }
+
+    if(this.walletStrList.includes(address)){
+      console.log(`valid address: ${address}`);
+      return true;
+    }
+
+    console.log(`not valid address: ${address}`);
+    return false;
+  }
+
+  getWalletBalance(address) {
+    if(!this.isValidWalletAddress(address)){
+      return;
+    }
+    for(var i=0; i<this.walletObjList.length; i++){
+      if(address == this.walletObjList[i].address) {
+        var balance = this.walletObjList[i].amount;
+        console.log(`${address}, balance: ${balance}`);
+        return balance; 
+      }
+    }
+    return;
+  }
+
+  updateWalletBalance(address, amount) {
+    if(!this.isValidWalletAddress(address)){
+      return;
+    }
+    for(var i=0; i<this.walletObjList.length; i++){
+      if(address == this.walletObjList[i].address) {
+        var startBalance = this.walletObjList[i].amount;
+        this.walletObjList[i].amount += parseInt(amount,10);
+        var endBalance = this.walletObjList[i].amount;
+        console.log(`${address}, startBalance: ${startBalance}, endBalance: ${endBalance}`);
+      }
+    }
+    return;
+  }
+
+  printVerbose() {
+    for(var i=0; i<this.walletObjList.length; i++) {
+      console.log(this.walletObjList[i]);
+    }
+    console.log(this.walletStrList);
+  }
+}
+
+// create the wallets
+var EXCHANGE = new Exchange();
+EXCHANGE.createNewWallet(100);
+EXCHANGE.createNewWallet(50);
+EXCHANGE.createNewWallet(75);
+EXCHANGE.printVerbose();
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
-  var addr = '_' + address;
-  const balance = balances[addr] || 0;
-  res.send({ balance });
+  if(!EXCHANGE.isValidWalletAddress(address)){
+    return;
+  }
+  const b = {
+    balance: EXCHANGE.getWalletBalance(address)
+  } 
+  res.send(b);
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  var addr_sender = '_' + sender;
-  var addr_recipient = '_' + recipient;
-  balances[addr_sender] -= amount;
-  balances[addr_recipient] = (balances[addr_recipient] || 0) + +amount;
-  res.send({ balance: balances[addr_sender] });
+  const {sender, recipient, signature, amount} = req.body;
+
+  if(!EXCHANGE.isValidWalletAddress(sender) || !EXCHANGE.isValidWalletAddress(recipient)){
+    return;
+  }
+
+  EXCHANGE.updateWalletBalance(sender, -amount);
+  EXCHANGE.updateWalletBalance(recipient, amount);
+  res.send({ balance: EXCHANGE.getWalletBalance(sender)});
 });
 
 app.listen(port, () => {
